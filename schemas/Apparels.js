9a043f645f396ca663502fe71558d88897e087d2
Anuvab Sen <anuvab.sen99@gmail.com>
2022-06-29 15:02:05 +0530

    Sanity Schema

---
+++ schemas/Apparels.js
@@ -0,0 +1,37 @@
+export default {
+    name: 'Apparels',
+    title: 'Apparels',
+    type: 'document',
+    fields: [
+      {
+        name: 'gender',
+        title: 'Gender',
+        type: 'string',
+      },
+      {
+        name: 'Color',
+        title: 'Color',
+        type: 'string',
+      },
+      {
+        name: 'size',
+        title: 'size',
+        type: 'string',
+      },
+      {
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+      },
+      {
+        name: 'countInStock',
+        title: 'countInStock',
+        type: 'number',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+      },   
+    ],
+  };
